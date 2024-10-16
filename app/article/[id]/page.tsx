30043dd1cb5a28ff29b6be0e29ecd5e6c470ccdf
Ricardo Almeida <ricardo2675@proton.me>
2024-10-16 12:24:11 +0100

    Created page for single article

--- app/article/[id]/page.tsx
+++ app/article/[id]/page.tsx
@@ -1,5 +1,33 @@
 "use server";
 
+import { articles } from "@/data/database";
+import { Article } from "@/types/article";
+
 export default async function Page({ params }: { params: { id: number } }) {
-    return <div>Article {params.id} </div>;
+    const article: Article | undefined = articles.find((article) => {
+        return article.id == params.id;
+    });
+
+    if (!article) {
+        return (
+            <div>
+                <h1>Article Not Found</h1>
+                <p>
+                    Sorry, we couldn&apos;t find the article you&apos;re looking
+                    for.
+                </p>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h1>{article.title}</h1>
+                <p>
+                    <strong>Author: </strong>
+                    {article.author}
+                </p>
+                <p>{article.body}</p>
+            </div>
+        );
+    }
 }
