07cbf2dcdc0c64b3b8a7188c4689598c83d0b689
Ricardo <ricardo2675@proton.me>
2024-12-02 23:31:44 +0000

    Fixed some more styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 "use server";
 
+import Header from "@/components/Header";
 import styles from "./page.module.css";
 import ArticleCard from "@/components/ArticleCard";
-import SearchBar from "@/components/SearchBar";
 import { prisma } from "@/prisma/prisma";
 import { Article } from "@prisma/client";
-import Link from "next/link";
 
 export default async function Home(props: {
     searchParams?: Promise<{ query?: string }>;
-}) {
+}): Promise<JSX.Element> {
     const searchParams = await props.searchParams;
     const query = searchParams?.query || "";
 
@@ -21,13 +20,7 @@ export default async function Home(props: {
 
     return (
         <div className={styles.container}>
-            <div className={styles.header}>
-                <h1>Articles</h1>
-                <div className={styles.dashboard}>
-                    <Link href={"/login"}>Login</Link>
-                    <SearchBar />
-                </div>
-            </div>
+            <Header />
             <div className={styles.articles}>
                 {filteredArticles.map((article, index: number) => (
                     <ArticleCard key={index} article={article} />
