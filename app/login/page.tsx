ba35ba4e9df63d4c5420fed5acd75e6463db785c
Ricardo <ricardo2675@proton.me>
2024-12-05 14:07:00 +0000

    Updated dependencies

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,50 +2,46 @@
 
 import { FormEvent, useState } from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
+import { z } from "zod";
 
-export default function Page() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+export default function Page(): JSX.Element {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
+
+    const formSchema = z.object({
+        email: z.string().email(),
+        password: z.string(),
+    });
 
     function handleSubmit(event: FormEvent<HTMLFormElement>): void {
         event.preventDefault();
-        console.log(username);
-        console.log(password);
+        const parsedData = formSchema.parse(formData);
+        console.log(parsedData);
     }
 
-    function handleUsernameChange(
-        event: React.ChangeEvent<HTMLInputElement>,
-    ): void {
-        setUsername(event.target.value);
-    }
-
-    function handlePasswordChange(
-        event: React.ChangeEvent<HTMLInputElement>,
-    ): void {
-        setPassword(event.target.value);
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <div className={styles.formContainer}>
-                <div className={styles.fieldContainer}>
-                    <input
-                        type="text"
-                        value={username}
-                        placeholder="Username"
-                        onChange={handleUsernameChange}
-                    ></input>
+            <div className={styles.form}>
+                <div className={styles.field}>
+                    <input className={styles.input} id="email" name="email" type="text" value={formData.email} placeholder="Email" onChange={handleChange} required></input>
                 </div>
-                <div className={styles.fieldContainer}>
-                    <input
-                        type="password"
-                        value={password}
-                        placeholder="Password"
-                        onChange={handlePasswordChange}
-                    ></input>
+                <div className={styles.field}>
+                    <input className={styles.input} id="password " name="password" type="password" value={formData.password} placeholder="Password" onChange={handleChange} required></input>
                 </div>
-                <button type="submit">Login</button>
-                No account? <a href="/signup">Sign Up</a>
+                <button className={styles.button} type="submit">Login</button>
+                <Link href="/signup">No account? Click here to sign up</Link>
             </div>
         </form>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,20 @@ import Header from "@/components/Header";
 import styles from "./page.module.css";
 import ArticleCard from "@/components/ArticleCard";
 import { prisma } from "@/prisma/prisma";
-import { Article } from "@prisma/client";
+import type { Article } from "@prisma/client";
 
-export default async function Home(props: {
-    searchParams?: Promise<{ query?: string }>;
-}): Promise<JSX.Element> {
+export default async function Home(props: { searchParams?: Promise<{ query?: string }>; }): Promise<JSX.Element> {
     const searchParams = await props.searchParams;
     const query = searchParams?.query || "";
 
-    const articles = await prisma.article.findMany();
-
-    const filteredArticles: Article[] = articles.filter((article: Article) =>
-        article.title.toLowerCase().includes(query.toLowerCase()),
-    );
+    const articles: ReadonlyArray<Article> = (await prisma.article.findMany()).filter((article: Article) =>
+        article.title.toLowerCase().includes(query.toLowerCase()));
 
     return (
         <div className={styles.container}>
             <Header />
             <div className={styles.articles}>
-                {filteredArticles.map((article, index: number) => (
+                {articles.map((article: Article, index: number) => (
                     <ArticleCard key={index} article={article} />
                 ))}
             </div>
