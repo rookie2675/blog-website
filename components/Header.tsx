039e211588cca6e06e897d65a1945be3127da65c
Ricardo <ricardo2675@proton.me>
2024-12-05 21:12:18 +0000

    Adjusted header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,13 @@
 import styles from "./Header.module.css";
 import Link from "next/link";
 import SearchBar from "./SearchBar";
+import { HomeIcon, PersonIcon } from "@primer/octicons-react";
 
 export default function Header(): JSX.Element {
     return (
         <div className={styles.header}>
-            <Link href="/">Home</Link>
-            <Link href={"/signup"}>Register</Link>
-            <Link href={"/login"}>Login</Link>
+            <Link href="/"> <HomeIcon className={styles.home} size={24} /></Link>
+            <Link href={"/login"}> <PersonIcon className={styles.login} size={24} /></Link>
             <SearchBar />
         </div>
     );
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -34,7 +34,7 @@ export default function SearchBar(): JSX.Element {
     return (
         <div className={styles.container}>
             <button className={styles.button} type="button" aria-label="Search" onClick={handleButtonClick}>
-                <SearchIcon size={20} />
+                <SearchIcon className={styles.icon} size={20} />
             </button>
             <form onSubmit={handleSubmit}>
                 {showInput && (
